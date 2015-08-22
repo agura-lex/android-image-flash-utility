9efc213cf89d030369c57718ab7ba7eac8f7d28a
agur4ik <agur4ik09@gmail.com>
2015-08-22 23:24:40 +0300

    Fix installation to /system/bin and /system/xbin

--- install-flash.sh
+++ install-flash.sh
@@ -8,9 +8,21 @@ PS3="
 Installation path: "
 select DEST in "/system/bin" "/system/xbin" "/data/local/bin"
 do
+	if [[ $DEST = "/system/bin" || $DEST = "/system/xbin" ]]
+	then
+		echo "Re-mounting system to read-write"
+		mount -o rw,remount /system || exit 1
+	fi
 	echo "Installing 'flash' to $DEST"
 	cp -f flash $DEST/ || exit 1
 	chmod 755 $DEST/flash
+	if [[ $DEST = "/system/bin" || $DEST = "/system/xbin" ]]
+	then
+		echo "Re-mounting system back to read-only"
+		mount -o ro,remount /system
+	else
+		echo 'Do not forget to add PATH=/data/local/bin:$PATH to your .bashrc file'
+	fi
 	break
 done
 echo "Done! Run 'flash' in your terminal to call the utility"
