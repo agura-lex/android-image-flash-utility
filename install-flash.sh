0b7d413961fedccd51145d025fb77878e18adea4
agur4ik <agur4ik09@gmail.com>
2015-08-22 19:44:07 +0300

    Aestetic fix (installer is still not tested)

--- install-flash.sh
+++ install-flash.sh
@@ -4,7 +4,7 @@ echo "Welcome to flash utility installer by Alexander Agura (agur4ik09@gmail.com
 echo
 echo "Select the installation path
 (/system/bin is recommended, data/local/bin requires using bash shell and adding line in .bashrc file)"
-PS3="Installation path"
+PS3="Installation path: "
 select DEST in "system/bin" "system/xbin" "data/local/bin"
 do
 	echo "Installing 'flash' to $DEST"
